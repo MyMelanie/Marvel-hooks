9547a69fde2ff2c0680ebb26e235c017ed4e4935
Natallia Karpuk <natalia.karpuk90@gmail.com>
2023-07-02 16:04:17 +0200

    commit full project

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,14 @@
+//import AppBanner from '../components/appBanner/AppBaner'
+import AppBanner from '../appBanner/AppBaner'
+//import ComicsList from '../components/comicsList/ComicsList'
+import ComicsList from '../comicsList/ComicsList'
+const ComicsPage = () => {
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList/>
+        </>
+    )
+}
+
+export default ComicsPage
